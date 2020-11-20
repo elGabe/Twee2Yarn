e6d3d54bb128c68fbd2ad68f9ff35a15e08217ee
elGabe <gabrield3d@outlook.com>
2020-11-19 19:38:08 -0500

    Deletes twine metadata

--- twee2yarn.py
+++ twee2yarn.py
@@ -15,6 +15,7 @@ new_string = copy_string
 #Open (or create) the output .yarn file
 output_file = open('output.yarn', 'w')
 
+# Generic function to replace a regex match with a defined value
 def regex_replace(regex, replacement_function):
     global copy_string
     global new_string
@@ -23,17 +24,26 @@ def regex_replace(regex, replacement_function):
     for m in matches:
         new_string = list(reg.sub(replacement_function(m), "".join(new_string)))
 
+# Replacing passage headers
 def passage_header(m):
     name = "".join(copy_string[m.start()+3:m.end()])
-    return "=== \n\ntitle: "+name +"\ntags: \n---\n\n"
+    return "=== \n\ntitle: "+ name +"\ntags: \n---\n\n"
 
-
+# Replacing speaker names
 def speaker_name(m):
     return "".join(copy_string[m.start()+1:m.end()-1])
 
+# Deleting Twine metadata
+def twine_metadata(m):
+    return ""
 
+# Replace syntax
 regex_replace(r'(\:\:\s?\w+)', passage_header)
 regex_replace(r'(\|\W?\w+\W+\|)', speaker_name)
+regex_replace(r'(\{"(.*")\})', twine_metadata)
+
+# Add the last node end character to the end of the file
+new_string = list("".join(new_string) + "\n===")
 
 output_file.write("".join(new_string))
 
